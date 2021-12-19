daeeb7afcf469e75158139f2d2e7522a08baea7b
Rawiah <Rawiah-000-@hotmail.com>
2021-12-19 09:11:46 +0300

    init

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser');
 var FastText = require('node-fasttext');
 const cors = require('cors');
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.use(cors());
+
 
 let config = { 
   dim: 100,
   input: "train.txt",
   output: "model"
 }
+
+
 
 FastText.train("supervised", config, function (success, error) {
 
@@ -19,32 +27,49 @@ FastText.train("supervised", config, function (success, error) {
   console.log(success)
   
 })
-
-app.use(cors())
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+var textItems = [];
+var labelsItems = [];
 
 app.get('/', (req, res) => {
-  res.sendfile("index.html");
+  res.render("index", {listOfTextItems : textItems, listOfLabelsItems : labelsItems});
 });
 
-app.get('/fasttext/', function(req, res) {
+
+app.post('/', function(req, res) {
   var statement = req.param('statement');
-    res.send(getFastTextResults(statement));
+  var item = req.body.statement;
+  
+  textItems.push(item);
+  labelsItems.push(getFastTextResults(statement));
+  res.redirect('/', {listOfTextItems : textItems}, {listOfLabelsItems : labelsItems});
 });
 
 function getFastTextResults(statement) {
 	//predict returns an array with the input and predictions for best cateogires
-	FastText.predict(
-		"model.bin", 3,
-		[statement],
-		function (success, error) {
 
-		  if(error) {
-			console.log(error)
-			return;
-		  }
-		  console.log(success)
-		})
-	return "success!";
+  var scsVar;
+
+  FastText.predict(
+    "model.bin", 3, [statement],
+    function(success, error){
+      if(error){
+        console.log(error)
+        return;
+      }
+      
+      console.log(success);
+      scsVar = success;
+    }
+  )
+  var items = [];
+  for (i=0; i< scsVar.length; i++){
+    let lbl = scsVar[i]["label"];
+    items.push(lbl.replace("__label__",""));
+  }
+  return items;
+
 }
 
 app.listen(8000, () => {
